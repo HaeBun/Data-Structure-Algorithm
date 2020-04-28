3c1d8b4300348ac6ca378ccfc2cf9463e2353269
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-28 22:27:29 +0900

    Chapter 01 - 연습문제 Q1 | 네 값의 최댓값을 구하는 max4 메서드를 작성하세요

---
+++ src/Chap01/Ch01_Q1.java
@@ -0,0 +1,33 @@
+package Chap01;
+
+import java.util.Scanner;
+
+class Ch01_Q1 {
+	static int max4(int a, int b, int c, int d) {
+		int max = a;
+		if(b>max)
+			max = b;
+		if(c>max)
+			max = c;
+		if(d>max)
+			max = d;
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		int a, b, c, d;
+		
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("네 정수의 최댓값을 구합니다.");
+		
+		System.out.print("a의 값 : "); a = stdIn.nextInt();
+		System.out.print("b의 값 : "); b = stdIn.nextInt();
+		System.out.print("c의 값 : "); c = stdIn.nextInt();
+		System.out.print("d의 값 : "); d = stdIn.nextInt();
+		
+		System.out.println("최댓값은 "+ max4(a, b, c, d) + "입니다");
+	}
+	
+}
