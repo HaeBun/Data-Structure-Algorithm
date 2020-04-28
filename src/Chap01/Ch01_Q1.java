312bec62cad025965688bf4d819941baa3b89859
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-28 22:30:01 +0900

    문제 Q1 | 네 값의 최댓값을 구하는 max4 메서드를 작성하세요.

--- src/Chap01/Ch01_Q1.java
+++ src/Chap01/Ch01_Q1.java
@@ -21,7 +21,6 @@ class Ch01_Q1 {
 		Scanner stdIn = new Scanner(System.in);
 		
 		System.out.println("네 정수의 최댓값을 구합니다.");
-		
 		System.out.print("a의 값 : "); a = stdIn.nextInt();
 		System.out.print("b의 값 : "); b = stdIn.nextInt();
 		System.out.print("c의 값 : "); c = stdIn.nextInt();
