5da3f0f68f2d88f860ea47554bb410cf5f9fcea9
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-29 19:53:31 +0900

    문제 Q9 | 정수 a,b를 포함하여 그 사이의 모든 정수의 합을 구하여 반환하는 아래 메서드를 작성하세요.

---
+++ src/Chap01/Ch01_Q9.java
@@ -0,0 +1,28 @@
+package Chap01;
+
+import java.util.Scanner;
+
+public class Ch01_Q9 {
+	static int sumof(int a, int b) {
+		int sum = 0;
+		
+		for(int i=a; i<=b; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		int a, b;
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("a부터 b까지의 합을 구합니다.");
+		System.out.print("a의 값: "); a = stdIn.nextInt();
+		System.out.print("b의 값: "); b = stdIn.nextInt();
+		
+		System.out.println(a+"부터 "+b+"까지의 합은 "+sumof(a,b)+"입니다.");
+		
+		
+		
+	}
+	
+}
