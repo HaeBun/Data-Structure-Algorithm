2dcc2e504e77bca8ce66a14116e997ab169c86e9
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-29 21:20:43 +0900

    문제 Q14 | 오른쪽과 같이 입력한 수를 한 변으로 하는 정사각형을 *기호로 출력하는 프로그램을 작성하세요.

---
+++ src/Chap01/Ch01_Q14.java
@@ -0,0 +1,20 @@
+package Chap01;
+
+import java.util.Scanner;
+
+class Ch01_Q14 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int n;
+		
+		System.out.println("사각형을 출력합니다.");
+		System.out.print("단 수 : ");
+		n = stdIn.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
